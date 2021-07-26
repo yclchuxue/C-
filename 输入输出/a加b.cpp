6a445875f7f40a28961a5d7010896ab1cdfc54fd
yclchuxue <3136165538@qq.com>
2021-07-26 21:48:57 +0800

    C++学习

---
+++ 输入输出/a加b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >>a>>b;
+
+    cout << "the sum of "<<a<<" and "<<b<<" is "<<a+b<<endl;
+
+    return 0;
+}
