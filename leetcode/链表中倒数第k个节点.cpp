5dbc5b1a266967a6faaafb1a3fed6df65e2b5d02
yclchuxue <3136165538@qq.com>
2021-09-02 19:55:34 +0800

    leetocde--剑指offer22

---
+++ leetcode/链表中倒数第k个节点.cpp
@@ -0,0 +1,75 @@
+/********************************
+ * 链表中倒数第k个节点--剑指offer22
+ * *****************************/
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+};
+
+class Solution 
+{
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) 
+    {
+        ListNode *p = head, *q = head;
+        int index = 0;      //前后指针相差数
+        while(p)
+        {
+            if(index == k)
+            {
+                break;
+            }
+            p = p->next;
+            ++index;
+        }
+        while(p)
+        {
+            p = p->next;
+            q = q->next;
+        }
+        return q;
+    }
+};
+
+int main()
+{
+    ListNode *head = nullptr, *tail = nullptr;
+    int value, k;
+    cin >> value;
+    while(value != -1)
+    {
+        ListNode *p = new ListNode;
+        p->val = value;
+        p->next = nullptr;
+        if(head == nullptr)
+        {
+            head = p;
+            tail = p;
+        }
+        else
+        {
+            tail->next = p;
+            tail = p;
+        }
+        cin >> value;
+    }
+    cout << "请输入一个小于链表长度的数：";
+    cin >> k;
+    Solution P;
+    ListNode *h = P.getKthFromEnd(head, k);
+    while(h)
+    {
+        cout << h->val << "\t";
+        h = h->next;
+    }
+    cout << endl;
+    return 0;
+}
