14f0b95e6e431c4a553ed5517ce3146694b4eee4
yclchuxue <3136165538@qq.com>
2022-03-10 21:26:34 +0800

    待完成

---
+++ leetcode/重构一棵树的方案数.cpp
@@ -0,0 +1,20 @@
+/*************************************
+ * 重构一棵树的方案数----------1719
+ * **********************************/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int checkWays(vector<vector<int>>& pairs) {
+
+    }
+};
+
+int main()
+{
+
+}
