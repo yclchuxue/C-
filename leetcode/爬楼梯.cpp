f716682df0cb1ec338a8cfcefa9114d18875fc6b
yclchuxue <3136165538@qq.com>
2022-05-27 23:41:25 +0800

    leetcode

---
+++ leetcode/爬楼梯.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q; 
+            q = r; 
+            r = p + q;
+        }
+        return r;
+    }
+};
