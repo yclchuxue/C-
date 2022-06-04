752f4a0308991f76bdaaaf0f9552e7b191c3929b
yclchuxue <3136165538@qq.com>
2022-06-04 21:41:11 +0800

    leetcode

---
+++ leetcode/K个一组翻转链表.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+
+    ListNode *reversalList(ListNode *head)
+    {
+        
+    }
+
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+
+    }
+};
+
+int main()
+{
+
+}
