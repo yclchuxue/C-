4b2f8f2ba53778302dc3e0985cce1106be3ded7c
yclchuxue <3136165538@qq.com>
2022-09-27 15:21:44 +0800

    leetcode

--- leetcode/最大正方形.cpp
+++ leetcode/最大正方形.cpp
@@ -27,4 +27,28 @@ public:
         int maxSquare = maxSide * maxSide;
         return maxSquare;
     }
+
+    int maximalSquare1(vector<vector<char>>& matrix) {
+        if(matrix.size() == 0 || matrix[0].size() == 0){
+            return 0;
+        }
+
+        int maxS = 0;
+        int rows = matrix.size(), columns = matrix[0].size();
+        vector<vector<int>> dp(rows, vector<int>(columns));
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                if(matrix[i][j] == '1'){
+                    if(i == 0 || j == 0){
+                        dp[i][j] = 1;
+                    }else{
+                        dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                    maxS = max(maxS, dp[i][j]);
+                }
+            }
+        }
+        return maxS*maxS;
+    }
 };
