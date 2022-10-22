5b975bb1cc7b63b095672fdec70ab52d6ab143a9
yclchuxue <3136165538@qq.com>
2022-10-22 21:39:18 +0800

    leetcode

--- leetcode/岛屿数量.cpp
+++ leetcode/岛屿数量.cpp
@@ -35,3 +35,54 @@ public:
         return num_islands;
     }
 };
+
+
+class Solution1{
+private:
+    int num;
+    int dfs(vector<vector<char>>& grid, int i, int j) {
+        cout << "grid[" << i << "][" << j << "]" << endl;
+        if(grid[i][j] == '0' || grid[i][j] == '2'){
+            cout << grid[i][j] << "over"<< endl;
+            return 0;
+        }else{
+            cout << "\t\t\tgrid[" << i << "][" << j << "] = " << grid[i][j] << endl;
+            grid[i][j] = '2';
+        }
+
+        if(i-1 >= 0)                dfs(grid, i-1, j);
+        if(i+1 < grid.size())       dfs(grid, i+1, j);
+        if(j-1 >= 0)                dfs(grid, i, j-1);
+        if(j+1 < grid[i].size())    dfs(grid, i, j+1);
+
+        return 1;
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        num = 0;
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                int ret = dfs(grid, i, j);
+                if(ret == 1){
+                    num++;
+                    cout << "num++" << endl;
+                }
+            }
+        }
+
+        return num;
+    }
+};
+
+int main(){
+    vector<vector<char>> grid = {
+        {'1','1','1'},
+        {'0','1','0'},
+        {'1','1','1'}
+    };
+
+    Solution1 P;
+
+    cout << P.numIslands(grid) << endl;
+}
