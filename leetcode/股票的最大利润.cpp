e851e421a528d16c9ad4454a9aecd8f558966110
yclchuxue <3136165538@qq.com>
2023-02-10 21:03:41 +0800

    leetcode

--- leetcode/股票的最大利润.cpp
+++ leetcode/股票的最大利润.cpp
@@ -23,3 +23,48 @@ public:
         return ret;
     }
 };
+
+class Solution1 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ret = 0, len = prices.size();
+        if(len < 2 )    return ret;
+
+        int min_pri = prices[0];
+
+        for(int i = 1; i < len; ++i){
+            if(prices[i] > min_pri){
+                ret = max(prices[i]-min_pri, ret);
+            }else{
+                ret = min(prices[i], min_pri);
+            }
+        }
+
+        return ret;
+    }
+};
+
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ret = 0, len = prices.size();
+        if(len < 2){
+            return ret;
+        }
+        
+        int min_price = prices[0], ret0;
+        for(int i = 1; i < len; i++){
+            if(prices[i] > min_price){
+                int x = prices[i]-min_price;
+                if(x > ret){
+                    ret0 = ret;
+                    ret = x;
+                }
+            }else{
+                min_price = min(prices[i], min_price);
+            }
+        }
+
+        return ret+ret0;
+    }
+};
